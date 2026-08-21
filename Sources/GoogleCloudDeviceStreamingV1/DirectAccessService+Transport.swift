36f2ca3696079c3fbf6e7ed7dee9071191354034
Carlos O'Ryan <coryan@google.com>
2026-08-21 21:36:45 +0000

    chore: update librarian circa 2028-08-21 (#507)
    This version uses `_QueryParameterEncoder()` which unblocks the removal
    of `QueryParameterEncoder()`.
    
    It also imports `GoogleCloudGax` with `@_spi()` so we can protect
    `GoogleCloudGax._RetryLoop`.

--- Sources/GoogleCloudDeviceStreamingV1/DirectAccessService+Transport.swift
+++ Sources/GoogleCloudDeviceStreamingV1/DirectAccessService+Transport.swift
@@ -44,7 +44,7 @@ extension Clients {
       var query = [
         URLQueryItem(name: "$alt", value: "json;enum-encoding=int")
       ]
-      let encoder = GoogleCloudGax.QueryParameterEncoder()
+      let encoder = GoogleCloudGax._QueryParameterEncoder()
       query.append(
         contentsOf: try encoder.encode(request.deviceSessionId, prefix: "deviceSessionId"))
       var req = try await self.inner.newRequest(path: path, query: query)
@@ -70,7 +70,7 @@ extension Clients {
       var query = [
         URLQueryItem(name: "$alt", value: "json;enum-encoding=int")
       ]
-      let encoder = GoogleCloudGax.QueryParameterEncoder()
+      let encoder = GoogleCloudGax._QueryParameterEncoder()
       query.append(contentsOf: try encoder.encode(request.pageSize, prefix: "pageSize"))
       query.append(contentsOf: try encoder.encode(request.pageToken, prefix: "pageToken"))
       query.append(contentsOf: try encoder.encode(request.filter, prefix: "filter"))
@@ -138,7 +138,7 @@ extension Clients {
       var query = [
         URLQueryItem(name: "$alt", value: "json;enum-encoding=int")
       ]
-      let encoder = GoogleCloudGax.QueryParameterEncoder()
+      let encoder = GoogleCloudGax._QueryParameterEncoder()
       query.append(contentsOf: try encoder.encode(request.updateMask, prefix: "updateMask"))
       var req = try await self.inner.newRequest(path: path, query: query)
       req.setMethod(.PATCH)
